37978b36318f8103eeaf8bb16cdd805579f5c787
datschef <barrycao2005@gmail.com>
2026-05-14 01:01:35 +1000

    added scanner and inputs for subjects' grades

--- wed-1305/studentGrades.java
+++ wed-1305/studentGrades.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class studentGrades {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Math grade: ");
+        double mathGrade = scanner.nextDouble();
+
+        System.out.print("Enter English grade: ");
+        double englishGrade = scanner.nextDouble();
+
+        System.out.print("Enter Literature grade: ");
+        double literatureGrade = scanner.nextDouble();
+    }
 }
